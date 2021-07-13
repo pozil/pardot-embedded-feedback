c540bc50e89719e72a8e6703e0e75e9e6d0fd3c7
pozil <philippe.ozil@gmail.com>
2021-07-13 15:53:50 +0200

    Moved to links and unified style

--- force-app/main/default/lwc/ratingFeedbackForm/ratingFeedbackForm.js
+++ force-app/main/default/lwc/ratingFeedbackForm/ratingFeedbackForm.js
@@ -1,8 +1,37 @@
 import { LightningElement, api } from "lwc";
 
 export default class RatingFeedbackForm extends LightningElement {
-  @api formUrl;
-  @api recipientEmail;
-  @api questionLabel;
-  @api prospectFieldName;
+  @api
+  formUrl;
+  @api
+  recipientEmail;
+  @api
+  questionLabel;
+  @api
+  prospectFieldName;
+
+  @api
+  get starCount() {
+    return this._starCount;
+  }
+  set starCount(value) {
+    this._starCount = parseInt(value, 10);
+    this.generateStars();
+  }
+
+  stars = [];
+
+  generateStars() {
+    const stars = [];
+    const baseUrl = `${this.formUrl}?email=${this.recipientEmail}&${this.prospectFieldName}=`;
+    console.log(baseUrl);
+    for (let i = this._starCount; i > 0; i--) {
+      const url = `${baseUrl}${i}`;
+      stars.push({
+        url,
+        value: i
+      });
+    }
+    this.stars = stars;
+  }
 }
